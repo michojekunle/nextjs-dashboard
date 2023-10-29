6075426763bacf761845d4539c49db6b37cea3f5
AMD <michojekunle1@gmail.com>
2023-10-29 22:34:29 +0100

    made changes

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,10 +1,13 @@
 'use server'
 
 import { z } from "zod";
+import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
 const InvoiceSchema = z.object({
     id: z.string(),
-    cuctomerId: z.string(),
+    customerId: z.string(),
     amount: z.coerce.number(),
     status: z.enum(['pending', 'paid']),
     date: z.string(),
@@ -13,11 +16,20 @@ const InvoiceSchema = z.object({
 const CreateInvoice = InvoiceSchema.omit({ id: true, date:true });
 
 export async function createInvoice(formData: FormData) {
-    const {  } = {
+    const { customerId, amount, status } = CreateInvoice.parse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-      };
-      // Test it out:
-      console.log(rawFormData);
+      });
+
+      const amountInCents = amount * 100;
+      const date = new Date().toISOString().split('T')[0];
+
+      await sql`
+                INSERT INTO invoices (customer_id, amount, status, date)
+                VALUES (${customerId}, ${amountInCents}, ${status}, ${date})  
+            `;
+
+      revalidatePath('/dashboard/invoices');
+      redirect('/dashboard/invoices');
 }
